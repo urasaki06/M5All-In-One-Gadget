d31b998e4fac399ebf80f6cafa73d24893815af9
urasaki_06 <system.06.2023@gmail.com>
2023-06-12 12:34:19 +0900

    初回コミット

---
+++ MdWBGTMonitor.h
@@ -0,0 +1,15 @@
+/* インクルードガード */
+#pragma once
+
+class MdWBGTMonitor // classの定義
+{
+private: // privateはクラス内からしかアクセスできない
+
+public: // publicはどこからでもアクセス可能
+/*DrTHSensor::init()　を実行し温湿度センサーの初期化を行う。*/
+
+
+/*温湿度を取得し、その値からWBGTを算出する。計算結果をもとにアラートの分類分けを行い	
+SAFE～DANGERの値をindexのアドレスに格納する。	*/
+
+};
